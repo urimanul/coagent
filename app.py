d06ccf2c96b60a310d0fc8c44688a7ddb1c7855d
urimanul <myopera9@gmail.com>
2025-02-12 13:45:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,28 @@ def search_emails(query):
 
 def create_calendar_event(date: str, time: str, duration: int):
     #title = st.input( .input("タイトルを入力してください")
-    title = "スケジュール"    
+    title = "スケジュール"
+
+    modal = Modal(
+    "Demo Modal", 
+    key="demo-modal",
+    
+    # Optional
+    padding=20,    # default value
+    max_width=744  # default value
+    )
+
+    open_modal = st.button("Open")
+    if open_modal:
+        modal.open()
+
+    if modal.is_open():
+        with modal.container():
+            st.write("Text goes here")
+
+            st.write("Some fancy text")
+            value = st.checkbox("Check me")
+            st.write(f"Checkbox checked: {value}")
 
     headers = {
         'SPOAuthentication': 'Hanipman',
