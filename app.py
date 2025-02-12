8733deeac8c7796e487a9720aa243a63f2a9ae74
urimanul <myopera9@gmail.com>
2025-02-12 16:32:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -394,3 +394,12 @@ if st.button("実行"):
     )
 
     st.write(responded)
+
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
