33bcf1ddb8a8edc30e3bc586ef73d15e0144d012
urimanul <myopera9@gmail.com>
2025-02-12 15:27:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,12 @@ co = cohere.ClientV2(api_key=os.environ.get("COHERE_API_KEY"))
 def vote(item):
     st.write(f"Why is {item} your favorite?")
     reason = st.text_input("Because...")
+    st.write(reason)
     if st.button("Submit"):
         st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()
 
-        return reason
+    return reason
 
 def unicode_unescape(data):
     if isinstance(data, dict):
