736e2a6ea6102951072b56bc32f03283b84bc6e1
urimanul <myopera9@gmail.com>
2025-02-12 14:16:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,13 +62,22 @@ def create_calendar_event(date: str, time: str, duration: int):
     #title = st.input( .input("タイトルを入力してください")
     title = "スケジュール"
 
-    @st.dialog("イベントタイトル")
+    @st.dialog("Cast your vote")
     def vote(item):
-        st.write(f"イベントタイトル")
-        title = st.text_input("イベントタイトルを入力してください。")
+        st.write(f"Why is {item} your favorite?")
+        reason = st.text_input("Because...")
         if st.button("Submit"):
-            st.session_state.vote = {"title": title}
+            st.session_state.vote = {"item": item, "reason": reason}
             st.rerun()
+
+    if "vote" not in st.session_state:
+        st.write("Vote for your favorite")
+        if st.button("A"):
+            vote("A")
+        if st.button("B"):
+            vote("B")
+    else:
+        f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
 
     headers = {
         'SPOAuthentication': 'Hanipman',
