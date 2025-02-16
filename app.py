1bcaf2fc8708b4a04bbb34348ddfb9906eadca17
urimanul <myopera9@gmail.com>
2025-02-16 09:49:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -119,14 +119,14 @@ def create_calendar_event(date: str, time: str, duration: int):
     }
     
     #start = date+' '+time
-    start = datetime.strptime(date+' '+time+':00', '%Y/%m/%d %H:%M:%S')
+    start = datetime.strptime(date+' '+time+':00', '%Y-%m-%d %H:%M:%S')
     #start = st.strftime('%Y/%m/%d %H:%M:%S')
     #sdatetime = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
     #end = date+' '+time+'+'+str(duration)
     
     dt = datetime.strptime(date+' '+time+':00', '%Y-%m-%d %H:%M:%S')
     new_dt = dt + timedelta(hours=duration)
-    end = new_dt.strftime('%Y/%m/%d %H:%M:%S')
+    end = new_dt.strftime('%Y-%m-%d %H:%M:%S')
     #end = datetime.strptime(str(new_dt), '%Y/%m/%d %H:%M:%S')
     
     st.write(start)
